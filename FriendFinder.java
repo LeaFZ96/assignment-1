e963fda13ecedc24162e7274810fff56a2081243
leafz <leafzhang714@gmail.com>
2022-07-04 23:43:39 -0400

    Fix typo

--- FriendFinder.java
+++ FriendFinder.java
@@ -5,63 +5,63 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FriendFinder {
-	
-	protected ClassesDataSource classesDataSource;
-	protected StudentsDataSource studentsDataSource;
-	
-	public FriendFinder(ClassesDataSource cds, StudentsDataSource sds) {
-		classesDataSource = cds;
-		studentsDataSource = sds;
-	}
-	
-	/*
-	 * This method takes a String representing the name of a student 
-	 * and then returns a set containing the names of everyone else 
-	 * who is taking the same classes as that student.
-	 */
-	public Set<String> findClassmates(Student theStudent) {
-		
-		String name = theStudent.getName();
-		
-		// find the classes that this student is taking
-		List<String> myClasses = classesDataSource.getClasses(name);
-		if (myClasses == null) {
-			return Collections.emptySet(); // return empty Set if the student isn't taking any classes
-		}
-		
-		// use the classes to find the names of the students
-		Set<String> classmates = new HashSet<String>();
-		
-		for (String myClass : myClasses) {
-			// list all the students in the class
-			List<Student> students = studentsDataSource.getStudents(myClass);
-			
-			for (Student otherStudent : students) {
-				
-				// find the other classes that they're taking
-				List<String> theirClasses = classesDataSource.getClasses(otherStudent.getName());
-							
-				// see if all of the classes that they're taking are the same as the ones this student is taking
-				boolean allSame = true;
-				for (String c : myClasses) {
-					if (theirClasses.contains(c) == false) {
-						allSame = false;
-						break;
-					}
-				}
-				
-				// if they're taking all the same classes, then add to the set of classmates
-				if (allSame) {
-					if (otherStudent.getName().equals(name) == false) {
-						classmates.add(otherStudent.getName());
-					}
-				}
-			}
+    
+    protected ClassesDataSource classesDataSource;
+    protected StudentsDataSource studentsDataSource;
+    
+    public FriendFinder(ClassesDataSource cds, StudentsDataSource sds) {
+        classesDataSource = cds;
+        studentsDataSource = sds;
+    }
+    
+    /*
+     * This method takes a String representing the name of a student 
+     * and then returns a set containing the names of everyone else 
+     * who is taking the same classes as that student.
+     */
+    public Set<String> findClassmates(Student theStudent) {
+        
+        String name = theStudent.getName();
+        
+        // find the classes that this student is taking
+        List<String> myClasses = classesDataSource.getClasses(name);
+        if (myClasses == null) {
+            return Collections.emptySet(); // return empty Set if the student isn't taking any classes
+        }
+        
+        // use the classes to find the names of the students
+        Set<String> classmates = new HashSet<String>();
+        
+        for (String myClass : myClasses) {
+            // list all the students in the class
+            List<Student> students = studentsDataSource.getStudents(myClass);
+            
+            for (Student otherStudent : students) {
+                
+                // find the other classes that they're taking
+                List<String> theirClasses = classesDataSource.getClasses(otherStudent.getName());
+                            
+                // see if all of the classes that they're taking are the same as the ones this student is taking
+                boolean allSame = true;
+                for (String c : myClasses) {
+                    if (theirClasses.contains(c) == false) {
+                        allSame = false;
+                        break;
+                    }
+                }
+                
+                // if they're taking all the same classes, then add to the set of classmates
+                if (allSame) {
+                    if (otherStudent.getName().equals(name) == false) {
+                        classmates.add(otherStudent.getName());
+                    }
+                }
+            }
 
-		}
-				
-		return classmates;
-	}
-	
+        }
+                
+        return classmates;
+    }
+    
 
 }
